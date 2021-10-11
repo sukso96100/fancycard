479b38078f6754e0f10daed4bfafb12d9578e4a8
Youngbin Han <youngbin.han@cloudmt.co.kr>
2021-10-11 14:51:19 +0900

    Add Render functions that fetch data from url parameters

--- router/router.go
+++ router/router.go
@@ -1,7 +1,48 @@
 package router
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sukso96100/fancycard/render"
+	"github.com/sukso96100/fancycard/tmpl"
+)
+
 func SetupRouter() {}
 
-func GetDataFromURL() {}
+func RenderWithDataFromURL(c *gin.Context) {
+	templatePath := c.DefaultQuery("template", "default")
+	queryParams := c.Request.URL.Query()
 
-func GetDataFromMetaTags() {}
+	templateText, err := tmpl.LoadTemplate(templatePath)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Template not found",
+		})
+		return
+	}
+	compiledTemplate, err := tmpl.CompileTemplate(templateText, queryParams)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	imageBuff, err := render.RenderImage(compiledTemplate)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	c.Data(200, "image/png", imageBuff)
+}
+
+func RenderWithDataFromMetaTags(c *gin.Context) {
+	targetURL := c.DefaultQuery("url", "")
+	if targetURL == "" {
+		c.JSON(400, gin.H{
+			"error": "Target URL is empty",
+		})
+	}
+
+}
