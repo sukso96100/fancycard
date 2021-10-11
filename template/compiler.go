39e418060d0995fd339fd6e5494428f1707057ee
Youngbin Han <youngbin.han@cloudmt.co.kr>
2021-10-11 12:57:03 +0900

    Add template loadind and compiler functions

--- template/compiler.go
+++
@@ -1,5 +0,0 @@
-package template
-
-func CompileTemplate() {
-
-}
---
+++ tmpl/compiler.go
@@ -0,0 +1,51 @@
+package tmpl
+
+import (
+	"bytes"
+	"embed"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"text/template"
+)
+
+// go:embed templates/*
+var BuiltInTemplates embed.FS
+
+// Loads template text from built in templates file or from URL.
+func LoadTemplate(templatePath string) (string, error) {
+	if strings.HasPrefix(templatePath, "http://") || strings.HasPrefix(templatePath, "https://") {
+		resp, err := http.Get(templatePath)
+		if err != nil {
+			return "", err
+		}
+
+		defer resp.Body.Close()
+
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+		return string(data), nil
+	}
+	tmplText, err := BuiltInTemplates.ReadFile(templatePath)
+	if err != nil {
+		return "", err
+	}
+	return string(tmplText), nil
+}
+
+// Compile the given template text with data into complete text output.
+func CompileTemplate(templateText string, data map[string]string) (string, error) {
+	tmpl, err := template.New("Template").Parse(templateText)
+	if err != nil {
+		return "", err
+	}
+
+	var compiled bytes.Buffer
+	err = tmpl.Execute(&compiled, data)
+	if err != nil {
+		return compiled.String(), err
+	}
+	return compiled.String(), nil
+}
