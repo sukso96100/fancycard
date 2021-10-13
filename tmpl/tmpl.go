86749b0035ad5f4aff919270622ff68ef90d4110
Youngbin Han <youngbin.han@cloudmt.co.kr>
2021-10-13 20:34:33 +0900

    Add ExtractMetaTagsFromURL

--- tmpl/tmpl.go
+++ tmpl/tmpl.go
@@ -4,14 +4,56 @@ import (
 	"bytes"
 	"embed"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"path"
 	"strings"
 	"text/template"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 //go:embed templates/*.html
 var builtInTemplates embed.FS
+
+// Extracts template path and map of data from meta tags from given web url.
+func ExtractMetaTagsFromURL(url string) (string, map[string][]string, error) {
+	// Request the HTML page.
+	res, err := http.Get(url)
+	if err != nil {
+		return "", nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return "", nil, err
+	}
+
+	templatePath := ""
+	templateData := make(map[string][]string)
+	metaNamePrefix := "fancycard:"
+
+	// Find meta tags with metaNamePrefix and build map of data.
+	doc.Find("meta").Each(func(i int, s *goquery.Selection) {
+		if name, _ := s.Attr("name"); strings.HasPrefix(name, metaNamePrefix) {
+			key := strings.TrimPrefix(name, metaNamePrefix)
+			value, _ := s.Attr("content")
+			if key == "template" && templatePath == "" {
+				templatePath = value
+			}
+			if key != "template" {
+				templateData[key] = append(templateData[key], value)
+			}
+		}
+	})
+
+	return templatePath, templateData, nil
+}
 
 // Loads template text from built in templates file or from URL.
 func LoadTemplate(templatePath string) (string, error) {
